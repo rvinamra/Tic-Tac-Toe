be680172916cd1b6fd8d43a4ab5724da7719d6da
Vinamra Rai <vinamravr1@gmail.com>
2017-12-10 11:25:43 +0530

    Add all files

---
+++ script.js
@@ -0,0 +1,155 @@
+var origBoard;
+const human = 'X';
+const ai = 'O';
+const winCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[6, 4, 2]
+]
+
+const cells = document.querySelectorAll('.cell');
+//Selects and stores all the cells of the table made in HTML file
+
+startGame();
+
+function startGame() {
+	document.querySelector(".endgame").style.display = "none"; //To check restart case
+	origBoard = Array.from(Array(9).keys());
+	//for loop follows to clear up the table at restart
+	
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].innerText = '';
+		cells[i].style.removeProperty('background-color');
+		//^^To remove the highlighted winning combo at restart
+		cells[i].addEventListener('click', turnClick, false);
+	}
+}
+
+//following functions helps in displaying the 'X' inserted by human player on click
+
+function turnClick(square) {
+	//The if block checks whether a square is empty by checking whether it 
+	//contains number or X/O and accordingly prevents the double-click scenario
+	//Moreover the checkTie makes sure that after all the squares are flled you no 
+	//longer could click and Tie Game mesaage could be displayed
+	
+	if (typeof origBoard[square.target.id] == 'number') {
+		turn(square.target.id, human)
+		if (!checkWin(origBoard, human) && !checkTie()) turn(bestSpot(), ai);
+	}
+}
+
+function turn(squareId, player) {
+	origBoard[squareId] = player;
+	document.getElementById(squareId).innerText = player;
+	let gameWon = checkWin(origBoard, player)
+	if (gameWon) gameOver(gameWon)
+}
+
+function checkWin(board, player) {
+	let plays = board.reduce((a, e, i) =>(e === player) ? a.concat(i) : a, []);
+	//Reduce method goes through every element of the board array, do something 
+	//and will return a value. a = accumulater, e = element; i = index
+	//finding every index, player has played in:
+	let gameWon = null;
+	for (let [index, win] of winCombos.entries()) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
+			gameWon = {index: index, player: player};
+			break;
+		}
+	}
+	return gameWon;
+	//checking if the entries given by the user satisfies the winning criterion
+}
+
+function gameOver(gameWon) {
+	for (let index of winCombos[gameWon.index]) {
+		document.getElementById(index).style.backgroundColor =
+			gameWon.player == human ? "blue" : "red";
+	}
+	for (var i = 0; i < cells.length; i++) {
+		cells[i].removeEventListener('click', turnClick, false);
+	}
+	declareWinner(gameWon.player == human ? "You win!" : "You lose.");
+}
+
+function declareWinner(who) {
+	document.querySelector(".endgame").style.display = "block";
+	document.querySelector(".endgame .text").innerText = who;
+}
+
+function emptySquares() {
+	return origBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+	return minimax(origBoard, ai).index;
+}
+
+function checkTie() {
+	if (emptySquares().length == 0) {
+		for (var i = 0; i < cells.length; i++) {
+			cells[i].style.backgroundColor = "green";
+			cells[i].removeEventListener('click', turnClick, false);
+		}
+		declareWinner("Tie Game!")
+		return true;
+	}
+	return false;
+}
+
+function minimax(newBoard, player) {
+	var availSpots = emptySquares(newBoard);
+
+	if (checkWin(newBoard, human)) {
+		return {score: -10};
+	} else if (checkWin(newBoard, ai)) {
+		return {score: 10};
+	} else if (availSpots.length === 0) {
+		return {score: 0};
+	}
+	var moves = [];
+	for (var i = 0; i < availSpots.length; i++) {
+		var move = {};
+		move.index = newBoard[availSpots[i]];
+		newBoard[availSpots[i]] = player;
+
+		if (player == ai) {
+			var result = minimax(newBoard, human);
+			move.score = result.score;
+		} else {
+			var result = minimax(newBoard, ai);
+			move.score = result.score;
+		}
+
+		newBoard[availSpots[i]] = move.index;
+
+		moves.push(move);
+	}
+
+	var bestMove;
+	if(player === ai) {
+		var bestScore = -10000;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score > bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	} else {
+		var bestScore = 10000;
+		for(var i = 0; i < moves.length; i++) {
+			if (moves[i].score < bestScore) {
+				bestScore = moves[i].score;
+				bestMove = i;
+			}
+		}
+	}
+
+	return moves[bestMove];
+}
